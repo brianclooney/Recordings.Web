bd0434a05850f6589d57997f03121f983a36668d
Brian <brian@dev-wnm-recordings.wnm.dev.home.lan>
2024-07-22 16:38:53 -0400

    Only load playlist when filter changes

--- UI/Models/TrackFilter.cs
+++ UI/Models/TrackFilter.cs
@@ -4,6 +4,23 @@ namespace Recordings.UI.Models
     public class TrackFilter
     {
         public string Value { get; set; } = string.Empty;
-        public TrackFilterType Type {get; set; } = TrackFilterType.None;
+        public TrackFilterType Type { get; set; } = TrackFilterType.None;
+
+        public override bool Equals(Object? o)
+        {
+            if (o == null || o is not TrackFilter)
+            {
+                return false;
+            }
+            else {
+                var f = (TrackFilter) o;
+                return Value == f.Value && Type == f.Type;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode() + Type.GetHashCode();
+        }
     }
 }
