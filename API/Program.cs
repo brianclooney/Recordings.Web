644999cf2720cbbd8809ae67736456fee0ff3d66
Brian <brian@dev-wnm-recordings.wnm.dev.home.lan>
2024-10-13 21:17:19 -0400

    Add database migration at API startup

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Recordings.API.Data;
 using Serilog;
 
 namespace Recordings.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Host.CreateDefaultBuilder(args)
-		.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration))
+            var host = Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration))
                 .ConfigureWebHostDefaults(wb => wb.UseStartup<Startup>())
-                .Build()
-                .Run();
+                .Build();
+
+                using var scope = host.Services.CreateScope();
+                await using var dbContext = scope.ServiceProvider.GetRequiredService<RecordingDbContext>();
+                try {
+                    await dbContext.Database.MigrateAsync();
+                }
+                catch (Exception e)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occurred during migration.");
+		    // return Task.Cpmleted;
+		    return;
+                }
+
+                await host.RunAsync();
         }
     }
 }
